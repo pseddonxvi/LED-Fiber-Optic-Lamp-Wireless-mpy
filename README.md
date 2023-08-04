e17da0a66d2c6f70a288d0ef51b0c959eb6f5383
lurbano <lurbano@earthsciweb.org>
2023-08-04 16:01:12 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# mLamps
+# Requirements:
+* Raspberry Pi Pico or Pico W
+
+## Install Circuitpython
+Download and copy the appropriate circuitpython file to the Raspberry Pi Pico (or pico W):
+* For Pico: https://circuitpython.org/board/raspberry_pi_pico/
+* For Pico W: https://circuitpython.org/board/raspberry_pi_pico_w/
+
+The Pico will rename itself CIRCUITPI
+
+## Copy files
+* Copy files and folders from this repository onto the Raspberry Pi.
+* The Pico will run the code.py file by default. 
+** This program assumes you have a 30 pixel array (at GPIO15) and a touch sensor (at GPIO16).
+** It will run a rainbow pattern that turns on and off when you touch the touch sensor.
---
+++ code.py
@@ -0,0 +1,44 @@
+
+import board
+import neopixel
+from ledPixelsPico import *
+import touchio
+
+nPix = 30
+
+pix = ledPixels(nPix, board.GP15)
+
+touch = touchio.TouchIn(board.GP16)
+print("Start touch", touch.value)
+
+switch = True
+
+while True:
+    
+    if switch:
+        
+        for j in range(255):
+            for i in range(pix.nPix):
+                pixel_index = (i * 256 // pix.nPix) + j
+                pix.pixels[i] = pix.wheel(pixel_index & 255, 0.5)
+            if touch.value:
+                switch = not switch
+                print("Touch", switch)
+                pix.setColor((0,0,0))
+        
+                time.sleep(0.5)
+                break
+            pix.pixels.show()
+            time.sleep(0.01)
+            
+    #time.sleep(0.5)
+    if touch.value:
+        switch = not switch
+        print("Touch On", switch)
+        time.sleep(0.5)
+    
+        
+    
+
+# pix.rainbowForever(speed=0.001)
+
